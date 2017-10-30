ebd75eb438bacc4cb66c972efd039aa4f24dc805
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-30 11:16:00 -0400

    Logo is now displayed.

--- Gardyn/components/Navbar/Navbar.js
+++ Gardyn/components/Navbar/Navbar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 export default class Navbar extends Component {
   render() {
     return (
       <View>
+        <Image
+          style={{width: 50, height: 50}}
+          source={require("../../images/leaf.png")}
+        />
         <Text>Hello world</Text>
       </View>
     );
