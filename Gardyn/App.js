39833ac913ec2f427b4f71d44e51fcca9bf6bc00
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-30 11:02:19 -0400

    Successfully displayed a component.

--- Gardyn/App.js
+++ Gardyn/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Navbar from "./components/Navbar";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Gardyn App</Text>
+        <Navbar />
       </View>
     );
   }
---
+++ Gardyn/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
