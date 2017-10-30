2de23a6e237f133b3b0158b682b72124f3fe961d
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-30 11:21:00 -0400

    Styling applied.

--- Gardyn/components/Navbar/Navbar.js
+++ Gardyn/components/Navbar/Navbar.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 export default class Navbar extends Component {
   render() {
     return (
       <View>
         <Image
-          style={{width: 50, height: 50}}
+          style={styles.logo}
           source={require("../../images/leaf.png")}
         />
-        <Text>Hello world</Text>
+        <Text style={styles.title}>Hello world</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 50, 
+    height: 50,
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+});
