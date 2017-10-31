7ed206c634802225b68e78e4a81f6fe2ebedb2f5
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-31 15:35:32 -0400

    Conditional rendering works.

--- App.js
+++ App.js
@@ -2,35 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-// import { Switch, Route, Router } from 'react-router-dom';
-import {
-  StackNavigator,
-} from 'react-navigation';
-/*
+
 export default class App extends React.Component {
+  state = {
+    displaying: "Home"
+  }
+
+  Body = () => {
+    if (this.state.displaying === "Home"){
+      return <Text>Body Test</Text>
+    }
+  }
+
   render() {
     return (
       <View>
         <Navbar 
-          style={styles.navbar}
-        />
-        <Router>
-        <Switch>
-          <Route exact path='/' component={AllPlants} />
-          <Route path='/plants/:name' component={AllPlants} />
-          <Route path='/guide/:name' component={AllPlants}/>
-        </Switch>
-        </Router>
+            style={styles.navbar}
+          />
+        {this.Body()}
       </View>
     );
   }
 }
-*/
-
-const App = StackNavigator({
-  Home: { screen: HomeScreen },
-  PlantPage: { screen: ProfileScreen },
-});
 
 const styles = StyleSheet.create({
   container: {
