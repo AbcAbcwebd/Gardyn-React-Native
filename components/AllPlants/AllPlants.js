fbda622e5f3cef5d0ddbf497272159c93c858ff5
Owen Essen <abc_abcwebd@yahoo.com>
2017-10-30 22:10:10 -0400

    Trying to troubleshoot map function issue.

--- components/AllPlants/AllPlants.js
+++ components/AllPlants/AllPlants.js
@@ -1,49 +1,104 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
-import axios from "axios";
-import PlantCard from "./components/PlantCard";
+import { Text, View, Image, StyleSheet, Button } from 'react-native';
+import PlantCard from "../PlantCard";
 
 export default class AllPlants extends Component {
   // This stores what plant data should be displayed
   // Includes dummy data in case the component loads before the data is retrieved. 
   state = {
-    plantData: [{plantName: "Loading...", _id: "000"}]
+    plantData: [{plantName: "Loading...", _id: "000"}, {random: "OBJ"}],
+    renderCall: false
   }
 
   // Retrieves plant data from the backend and stores it in state. 
   retrievePlants = () => {
-    // To ensure context of 'this' isn't lost inside the axios function. 
+    // To ensure context of 'this' isn't lost inside the API function. 
     const parentObj = this;
-    axios.get('/api/plants')
+    fetch('https://gardyn.herokuapp.com/api/plants')
         .then(function (response) {
+          console.log("About to log response")
+          console.log(response._bodyText)
           parentObj.setState({
-            plantData: response.data
+            plantData: response._bodyText
           })
         })
         .catch(function (error) {
           console.log(error);
         });
   }
-
+/*
   // Before the component can load the retrieveCrops function is executed in order to retrieve data from the backend. 
   componentWillMount() {
       this.retrievePlants();
+  }
+*/
+  printState = () => {
+    console.log("Function hit")
+    console.log(this.state.plantData)
+  }
+
+  loadContent = () => {
+    this.state.renderCall = true;
+  }
+
+  hideContent = () => {
+    this.state.renderCall = false;
   }
 
   render() {
     return (
       <View>
         {
+          this.state.renderCall ? (
             this.state.plantData.map(item => (
               <PlantCard
                 item = {item}
               />
             ))
-          }
+          ) : (
+            <Text>Waiting to load</Text>
+          )
+        }
+          <Button
+            onPress={this.printState}
+            title="Check State"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            onPress={this.retrievePlants}
+            title="API Call"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            onPress={this.loadContent}
+            title="Run Map Function"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          <Button
+            onPress={this.hideContent}
+            title="Hide Contents"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+      
       </View>
     );
   }
 }
+
+/*
+
+
+          <Button
+            onPress={this.printState}
+            title="Check State"
+            color="#841584"
+            accessibilityLabel="Learn more about this purple button"
+          />
+          */
 
 const styles = StyleSheet.create({
   logo: {
